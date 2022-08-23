fc519ac218cf11910dec2084cc5f2f33be78612e
Pritish <maitypritish28@gmail.com>
2022-08-24 00:15:08 +0530

    add moment js

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -3,18 +3,27 @@ import { WiThermometer } from 'react-icons/wi'
 import { WiHumidity } from 'react-icons/wi'
 import { WiDayWindy } from 'react-icons/wi'
 import { WiBarometer } from 'react-icons/wi'
+import moment from 'moment';
 import '../App.css'
 
 const Weather = ({ data }) => {
     return (
         <div>
-            <div className='d-flex justify-content-center flex-column  flex-row  m-5'>
-                <div className='d-flex justify-content-start my-3'>
-                    <div><img style={{ height: '200px', width: '200px' }} src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} alt="wthr img" /></div>
-                    <div className='fs-1 fw-bold my-5'>{data.weather[0].description}</div>
+            <div className='d-flex justify-content-center flex-column  m-5'>
+                <div className='d-flex justify-content-evenly flex-row my-3'>
+                    <div className='fs-1 fw-bold my-5'>{data.name}</div>
+                    <div className='d-flex flex-column'>
+                        <img style={{ height: '200px', width: '200px' }} src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} alt="wthr img" />
+                        <span>{data.weather[0].description}</span>
+                    </div>
+                    <div className="date-time">
+                        <div className="day">{moment().format('dddd')}</div>
+                        <div className="date">{moment().format('LL')}</div>
+                    </div>
+
                 </div>
 
-                <div className='d-flex justify-content-start'>
+                <div className='d-flex  justify-content-center align-items-center '>
                     <div className='weather-box d-flex lign-items-center flex-column mx-3 p-3'>
                         <WiThermometer style={{ height: '100px', width: '100px' }} />
                         <div className='fs-1 fw-bold'>{Math.round(data.main.temp - 273)}°C</div>
